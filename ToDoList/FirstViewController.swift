03e12f62ca5166ce4c9137085d2f64291c1b7481
atkallie <kal17da@gmail.com>
2017-01-31 13:36:21 -0500

    Initial Commit

--- ToDoList/FirstViewController.swift
+++ ToDoList/FirstViewController.swift
@@ -8,16 +8,57 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet var table: UITableView!
 
+    var items: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        
+        let itemsObj = UserDefaults.standard.object(forKey: "myList")
+        
+        if let tempItems = itemsObj as? [String] {
+            
+            items = tempItems
+            
+        }
+        
+        table.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return items.count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = items[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == UITableViewCellEditingStyle.delete{
+            items.remove(at: indexPath.row)
+            
+            table.reloadData()
+            
+            UserDefaults.standard.set(items, forKey: "myList")
+        }
     }
 
 
--- ToDoList/SecondViewController.swift
+++ ToDoList/SecondViewController.swift
@@ -8,8 +8,29 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
-
+class SecondViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet var task: UITextField!
+    @IBAction func addTask(_ sender: Any) {
+        
+        let itemsObj = UserDefaults.standard.object(forKey: "myList")
+        var items: [String]
+        
+        if let itemsTemp = itemsObj as? [String]{
+            
+            items = itemsTemp
+            
+            items.append(task.text!)
+            
+        }else{
+            
+            items = [task.text!]
+            
+        }
+        
+        UserDefaults.standard.set(items, forKey: "myList")
+        
+        task.text = ""
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +39,17 @@ class SecondViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        addTask(Any.self)
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
 
 
